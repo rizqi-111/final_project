7ae05a7085cb8e3c0e50bdd225df0a7a53695ec5
Adjie Arya Sandjaya Wardana <aji17902@gmail.com>
2020-08-13 20:49:39 +0700

    Menambah Auth dan Menambah UI

---
+++ resources/views/adminlte/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  @include('adminlte.partials._css')
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+  <!-- Navbar -->
+  @include('adminlte.partials.navbar')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('adminlte.partials.sidebar')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">   
+
+      <!-- Default box -->
+      @yield('content')
+      <!-- /.card -->
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  @include('adminlte.partials.footer')
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+@include('adminlte.partials._js')
+@stack('scripts')
+</body>
+</html>
